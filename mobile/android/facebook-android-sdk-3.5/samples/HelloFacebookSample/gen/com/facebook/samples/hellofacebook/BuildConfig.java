95d4a194fbb2a604de1168f0e3fa86e467a1abe3
Oleg Dats <tezavrat@gmail.com>
2014-08-06 20:33:41 +0300

    Move Android to Gradle build system

--- mobile/android/facebook-android-sdk-3.5/samples/HelloFacebookSample/gen/com/facebook/samples/hellofacebook/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.facebook.samples.hellofacebook;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- mobile/android/facebook-android-sdk-3.5/samples/Scrumptious/gen/com/facebook/scrumptious/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.facebook.scrumptious;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
