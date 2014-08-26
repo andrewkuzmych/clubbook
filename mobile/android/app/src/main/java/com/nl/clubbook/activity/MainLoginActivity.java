72bd28b8c35060d08e33b29df8392c2fdf2e5420
volodymyryatsykiv <volodymyr.yatsykiv@techmagic.co>
2014-08-26 12:41:32 +0300

    1. Implemented tabs for sorting club by distance and name.

--- mobile/android/app/src/main/java/com/nl/clubbook/activity/MainLoginActivity.java
+++ mobile/android/app/src/main/java/com/nl/clubbook/activity/MainLoginActivity.java
@@ -3,7 +3,6 @@ package com.nl.clubbook.activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 
