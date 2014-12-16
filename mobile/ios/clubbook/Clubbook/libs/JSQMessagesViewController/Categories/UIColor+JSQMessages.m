98015056f3f7ab7003b3ac9df308a20575cc54d1
Anton Semenyuk <anton.semenyuk@gmail.com>
2014-12-16 16:38:25 +0200

    Revert "Removed old version of JSQMessageController"
    This reverts commit be0fea23e8eb105c36f9b4a8e292469e8aa10b9f.

---
+++ mobile/ios/clubbook/Clubbook/libs/JSQMessagesViewController/Categories/UIColor+JSQMessages.m
@@ -0,0 +1,76 @@
+//
+//  Created by Jesse Squires
+//  http://www.hexedbits.com
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/JSQMessagesViewController
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQMessagesViewController
+//
+//
+//  License
+//  Copyright (c) 2014 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+#import "UIColor+JSQMessages.h"
+
+@implementation UIColor (JSQMessages)
+
+#pragma mark - Message bubble colors
+
++ (UIColor *)jsq_messageBubbleGreenColor
+{
+    return [UIColor colorWithRed:54/255.0 green:217/255.0 blue:0/255.0 alpha:1.0];
+}
+
++ (UIColor *)jsq_messageBubbleBlueColor
+{
+    return [UIColor colorWithHue:210.0f / 360.0f
+                      saturation:0.94f
+                      brightness:1.0f
+                           alpha:1.0f];
+}
+
++ (UIColor *)jsq_messageBubbleLightGrayColor
+{
+    return [UIColor colorWithRed:156/255.0 green:156/255.0 blue:156/255.0 alpha:1.0];
+}
+
+#pragma mark - Utilities
+
+- (UIColor *)jsq_colorByDarkeningColorWithValue:(CGFloat)value
+{
+    NSUInteger totalComponents = CGColorGetNumberOfComponents(self.CGColor);
+    BOOL isGreyscale = (totalComponents == 2) ? YES : NO;
+    
+    CGFloat *oldComponents = (CGFloat *)CGColorGetComponents(self.CGColor);
+    CGFloat newComponents[4];
+    
+    if (isGreyscale) {
+        newComponents[0] = oldComponents[0] - value < 0.0f ? 0.0f : oldComponents[0] - value;
+        newComponents[1] = oldComponents[0] - value < 0.0f ? 0.0f : oldComponents[0] - value;
+        newComponents[2] = oldComponents[0] - value < 0.0f ? 0.0f : oldComponents[0] - value;
+        newComponents[3] = oldComponents[1];
+    }
+    else {
+        newComponents[0] = oldComponents[0] - value < 0.0f ? 0.0f : oldComponents[0] - value;
+        newComponents[1] = oldComponents[1] - value < 0.0f ? 0.0f : oldComponents[1] - value;
+        newComponents[2] = oldComponents[2] - value < 0.0f ? 0.0f : oldComponents[2] - value;
+        newComponents[3] = oldComponents[3];
+    }
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+	CGColorRef newColor = CGColorCreate(colorSpace, newComponents);
+	CGColorSpaceRelease(colorSpace);
+    
+	UIColor *retColor = [UIColor colorWithCGColor:newColor];
+	CGColorRelease(newColor);
+    
+    return retColor;
+}
+
+@end
